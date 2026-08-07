e9f634d99119461419433126a26ac6103e40226a
Richmond Owusu <richmowusu44@gmail.com>
2026-08-07 12:26:19 -0400

    handle cart updated

--- src/ProductList.jsx
+++ src/ProductList.jsx
@@ -1,10 +1,23 @@
 import React, { useState, useEffect } from 'react';
 import './ProductList.css'
 import CartItem from './CartItem';
+import addItem from './CartSlice';
 function ProductList({ onHomeClick }) {
     const [showCart, setShowCart] = useState(false);
     const [showPlants, setShowPlants] = useState(false); // State to control the visibility of the About Us page
+    const [addedToCart, setAddedToCart] = useState({});
 
+    const dispatch = useDispatch();
+    
+    const handleAddToCart = (product) => {
+        dispatch(addItem(product)); // Dispatch the action to add the product to the cart (Redux action)
+      
+        setAddedToCart((prevState) => ({ // Update the local state to reflect that the product has been added
+          ...prevState, // Spread the previous state to retain existing entries
+          [product.name]: true, // Set the current product's name as a key with value 'true' to mark it as added
+        }));
+      };
+      
     const plantsArray = [
         {
             category: "Air Purifying Plants",
